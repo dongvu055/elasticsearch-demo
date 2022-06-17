da94ddc1cc3cb4215ee8d36739234746e6d23ffe
Dong <dongvu>
2022-06-17 13:28:46 +0700

    update

--- src/main/java/com/example/elasticsearchexample/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/com/example/elasticsearchexample/elasticsearch/ElasticsearchConfig.java
@@ -1,9 +1,5 @@
 package com.example.elasticsearchexample.elasticsearch;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,16 +14,6 @@ public class ElasticsearchConfig {
 
     @Value("${elasticsearch.port}")
     private int port;
-
-    @Bean
-    public ElasticsearchClient elasticsearchClient() {
-        RestClient restClient = RestClient.builder(new HttpHost(host, port)).build();
-
-        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
-
-        return new ElasticsearchClient(transport);
-    }
-
     @Bean
     public RestClient restClient() {
         return RestClient.builder(new HttpHost(host, port)).setRequestConfigCallback(builder -> builder
